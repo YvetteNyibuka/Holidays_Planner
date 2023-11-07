caa684ac09c318aa44ed07edbb797558818b293a
YvetteNyibuka <izanyibukayvette@gmail.com>
2023-11-07 20:56:09 +0200

    Creating new Booking

--- src/components/TourDetail.jsx
+++ src/components/TourDetail.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "../styles/TourDetail.css";
-import destinatio from "../../public/images/destination-img2.jpg";
 import { BsCheckLg } from "react-icons/bs";
 import ToorUse from "./ToorUse";
 import { useParams } from "react-router-dom";
 import { usestatecontext } from "../context/ContextProvider";
 import axios from "axios";
+
 const TourDetail = () => {
   const [isCheck, setIsCheck] = useState(false);
   const { loggedUser } = usestatecontext();
@@ -35,15 +35,45 @@ const TourDetail = () => {
   const handleIsCheck = () => {
     setIsCheck((check) => !check);
   };
-
+let yvette= tourr._id;
+console.log("ssssssssssssssssssssss", yvette);
   const [booktour, setbooktour] = useState({
     bookingDate: "",
     paymentMethod: "",
     ticketCount: "",
     messagee: "",
+    // tourID: tourr._id,
+    fullname: loggedUser.fullName,
+    email: loggedUser.email,
+    confirmEmail: loggedUser.email,
+    phone: "07888888",
+    tourID: id,
   });
-    const handleBook = (e) => {
+  const handleBook = (e) => {
     setbooktour({ ...booktour, [e.target.name]: e.target.value });
+  };
+
+  let user = JSON.parse(localStorage.getItem("info"));
+  let token = user?.access_token;
+  // let userData = user?.user;
+  console.log(token);
+  const handlebookSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        "https://holiday-planner-4lnj.onrender.com/api/v1/booking/create",
+        booktour,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTQxZmJjMjEzZTMyZDA3NTVjMTI2NTUiLCJpYXQiOjE2OTkzNzkzMDYsImV4cCI6MTY5OTM5MDEwNn0.joe6vA9qh7ZVduxzwV37agsgTVRBDetHmP3HS8BomyU",
+          },
+        }
+      );
+      alert("booking created");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -78,7 +108,7 @@ const TourDetail = () => {
               </div> */}
             </div>
             <div className="tttable">
-              <div className="div2">{tourr.Duration} Months</div>
+              <div className="div2">{tourr.Duration}</div>
               <div className="div2">{tourr.GroupSize}</div>
               <div className="div2">{tourr.Seats}</div>
               <div className="div2">{tourr.destination}</div>
@@ -170,13 +200,20 @@ const TourDetail = () => {
           <div className="relativediv">
             <form className="formdiv">
               <div className="field1">
-                <input
+                {/* <input
                   type="text"
                   className="formfield"
                   placeholder="userId*"
                   name="userId"
                   value={loggedUser?._id}
-                ></input>
+                ></input> */}
+                {/* <input
+                  type="text"
+                  className="formfield"
+                  placeholder="tourId*"
+                  name="tourID"
+                  value={tourr?._id}
+                ></input> */}
               </div>
               <div className="field1">
                 <input
@@ -191,9 +228,9 @@ const TourDetail = () => {
                 <input
                   type="text"
                   className="formfield"
-                  placeholder="tourId*"
-                  name="tourId"
-                  value={tourr?._id}
+                  placeholder="email*"
+                  name="emil"
+                  value={loggedUser?.email}
                 ></input>
               </div>
               <div className="field1">
@@ -209,7 +246,7 @@ const TourDetail = () => {
                 <input
                   type="date"
                   className="formfield"
-                  placeholder="mm/dd/yy*"
+                  // placeholder="yy/mm/dd*"
                   name="bookingDate"
                   value={booktour.bookingDate}
                   onChange={handleBook}
@@ -251,7 +288,11 @@ const TourDetail = () => {
                 </div>
                 <p>Check Availability</p>
               </div>
-              <button type="submit" className="buttoned">
+              <button
+                type="submit"
+                className="buttoned"
+                onClick={handlebookSubmit}
+              >
                 BOOO NOW
               </button>
             </form>
